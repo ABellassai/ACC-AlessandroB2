45cdc16d7701fac7367f20fbe56c4b375b26f2f4
PizzaGuy07 <92167252+PizzaGuy07@users.noreply.github.com>
2021-10-08 14:57:04 +0100

    Add files via upload

---
+++ Computer Science 07.10.2021.py
@@ -0,0 +1,55 @@
+#1
+print('Welcome! :)');
+firstName = input('Enter your first name: ');
+surName = input('Enter your surname: ');
+print ('Welcome', firstName, surName,' ;)');
+
+#2
+ageNumber = int(input('What is your age? '));
+print('Welcome', firstName, surName,'You are ', ageNumber);
+print('You are going to be', ageNumber+1, 'in a year');
+
+#3
+heightNumber = float(input('What is your height? (Meters)'));
+print('Your height is', heightNumber' meters');
+
+#4
+firstNumber = int(input('Number 1:'));
+secondNumber = int(input('Number 2:'));
+print ('Number 1 + Number 2 is:',firstNumber + secondNumber);
+
+#5
+firstNumber = int(input('Number 1:'));
+secondNumber = int(input('Number 2:'));
+print ('Number 1 - Number 2 is:',firstNumber - secondNumber);
+
+#6
+firstNumber = int(input('Number 1:'));
+secondNumber = int(input('Number 2:'));
+print ('Number 1 X Number 2 is:',firstNumber * secondNumber);
+
+#7
+firstNumber = int(input('Number 1:'));
+secondNumber = int(input('Number 2:'));
+print ('Number 1 divided Number 2 is:',firstNumber / secondNumber);
+
+#8
+firstNumber = int(input('Number 1:'));
+secondNumber = int(input('Number 2:'));
+print ('Number 1 with Number 2 is:',firstNumber // secondNumber);
+
+#9
+firstNumber = int(input('Number 1:'));
+secondNumber = int(input('Number 2:'));
+print ('Number 1 with Number 2 is:',firstNumber % secondNumber);
+
+#10 a
+firstNumber = int(input('Number 1:'));
+secondNumber = int(input('Number 2:'));
+thirdNumber = int(input('Number 3:'));
+print ('The average between Number 1, Number 2, Number 3 is:',(firstNumber + secondNumber + thirdNumber) /3);
+
+#10 b
+print ('Number 1 was: ',firstNumber);
+print ('Number 2 was: ',secondNumber);
+print ('Number 3 was: ',thirdNumber);
