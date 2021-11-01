ee94f12e08672a1e4265ee75be7999a1a846234f
ABellassai.acc <92167252+ABellassai@users.noreply.github.com>
2021-11-01 22:11:20 +0000

    Update CS If While Worksheet 5.py

--- CS If While Worksheet 5.py
+++ CS If While Worksheet 5.py
@@ -1,4 +1,5 @@
-#Number Game
+#Number Game      Q1
+import math
 import random
 integerA = int(input('Enter a number: '))
 integerB = int(input('Enter another number: '))
@@ -27,5 +28,48 @@ if answer == str(randomChoice):
     print('Correct, your number was ' + str(randomChoice))
 else:
     print('You are out of guesses looser AHAHAHAHA!')
+    print('The number the computer picked was',randomChoice)
 
-#Credit Card Program
+    
+#Credit Card Program     Q2
+cardNum = ""
+while type(cardNum) == str or len(str(cardNum)) > 16 or len(str(cardNum)) <= 13:
+    try:
+        cardNum = int(input("Please enter a valid credit card number. This number must be between 13 and 16 digits and contain no letters or decimals."))
+    except ValueError:
+        pass
+cardNum = str(cardNum)
+if cardNum[:1] == "4":
+    print("This is a Visa card.")
+elif cardNum[:1] == "5":
+    print("This is a Master card")
+elif cardNum[:2] == "37":
+    print("This is an American Express card.")
+elif cardNum[:1] == "6":
+    print("This is a Discover card.")
+else:
+    print("This not a valid number.")
+    exit()
+    
+counter = len(cardNum)-2
+evens = 0
+while counter >= 0:
+    evenNo = int(cardNum[counter])*2
+    if len(str(evenNo)) > 1:
+        evenNo = int(str(evenNo)[0]) + int(str(evenNo)[1])
+    evens += evenNo
+    counter -=2
+
+counter = len(cardNum)-1
+odds = 0
+while counter >= 0:
+    odds += int(cardNum[counter])
+    counter -=2
+    
+
+if (evens + odds) % 10 == 0:
+    print("This card is valid.")
+else:
+    print("This card is not valid.")
+
+
