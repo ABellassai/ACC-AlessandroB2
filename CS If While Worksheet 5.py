8dfcd9d2a5a3a4f08420129cf4c4570eb7bb9c73
ABellassai.acc <92167252+ABellassai@users.noreply.github.com>
2021-11-01 22:16:39 +0000

    Update CS If While Worksheet 5.py

--- CS If While Worksheet 5.py
+++ CS If While Worksheet 5.py
@@ -32,44 +32,42 @@ else:
 
     
 #Credit Card Program     Q2
-cardNum = ""
-while type(cardNum) == str or len(str(cardNum)) > 16 or len(str(cardNum)) <= 13:
+cardNumber = ""
+while type(cardNumber) == str or len(str(cardNumber)) > 16 or len(str(cardNumber)) <= 13:
     try:
-        cardNum = int(input("Please enter a valid credit card number. This number must be between 13 and 16 digits and contain no letters or decimals."))
+        cardNumber = int(input('Enter a valid credit card number which must be between 13 and 16 digits and cannot contain letters or decimals'))
     except ValueError:
         pass
-cardNum = str(cardNum)
-if cardNum[:1] == "4":
-    print("This is a Visa card.")
-elif cardNum[:1] == "5":
-    print("This is a Master card")
-elif cardNum[:2] == "37":
-    print("This is an American Express card.")
-elif cardNum[:1] == "6":
-    print("This is a Discover card.")
+cardNumber = str(cardNumber)
+if cardNumber[:1] == '4':
+    print('This is a Visa card')
+elif cardNumber[:1] == '5':
+    print('This is a Master card')
+elif cardNumber[:2] == '37':
+    print('This is an American Express card')
+elif cardNumber[:1] == '6':
+    print('This is a Discover card')
 else:
-    print("This not a valid number.")
+    print('This is not a valid number')
     exit()
     
-counter = len(cardNum)-2
+counter = len(cardNumber)-2
 evens = 0
 while counter >= 0:
-    evenNo = int(cardNum[counter])*2
+    evenNo = int(cardNumber[counter])*2
     if len(str(evenNo)) > 1:
         evenNo = int(str(evenNo)[0]) + int(str(evenNo)[1])
     evens += evenNo
     counter -=2
 
-counter = len(cardNum)-1
+counter = len(cardNumber)-1
 odds = 0
 while counter >= 0:
-    odds += int(cardNum[counter])
+    odds += int(cardNumber[counter])
     counter -=2
-    
 
 if (evens + odds) % 10 == 0:
     print("This card is valid.")
 else:
     print("This card is not valid.")
-
-
+    
