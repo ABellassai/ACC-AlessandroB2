5c46ce5b72b3718cfde887d68e2cd7c1261f7bb7
ABellassai.acc <92167252+ABellassai@users.noreply.github.com>
2021-11-08 23:12:48 +0000

    Add files via upload

---
+++ CS For Loops Worksheet 6.py
@@ -0,0 +1,25 @@
+
+#1
+'''for i in range (11):
+    print(i)'''
+
+#2
+for i in range (10, 21, +1):
+    if (i % 2 == 0):
+        print(i + i)
+    
+#3
+'''for i in range (10, 0, -1):
+    if (i % 2 == 0):
+        print(i)'''
+
+#4
+
+
+#5
+
+
+#6
+
+
+#7
