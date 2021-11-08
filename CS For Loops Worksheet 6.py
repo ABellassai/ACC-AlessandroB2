efc6ae0d25ca6fa4688a0ec58bb6250cf0da3b87
ABellassai.acc <92167252+ABellassai@users.noreply.github.com>
2021-11-08 23:17:34 +0000

    Update CS For Loops Worksheet 6.py

--- CS For Loops Worksheet 6.py
+++ CS For Loops Worksheet 6.py
@@ -1,7 +1,7 @@
 
 #1
-'''for i in range (11):
-    print(i)'''
+for i in range (11):
+    print(i)
 
 #2
 for i in range (10, 21, +1):
@@ -9,12 +9,13 @@ for i in range (10, 21, +1):
         print(i + i)
     
 #3
-'''for i in range (10, 0, -1):
+for i in range (10, 0, -1):
     if (i % 2 == 0):
-        print(i)'''
+        print(i)
 
 #4
-
+for i in range (11):
+        print(i, 'squared is:', i*i, ', and cubed is:', i*i*i)
 
 #5
 
