309f307241b3fe1294288e8bce6e9b9af4e8ce2d
ABellassai.acc <92167252+ABellassai@users.noreply.github.com>
2021-11-10 14:41:16 +0000

    Update CS For Loops Worksheet 6.py

--- CS For Loops Worksheet 6.py
+++ CS For Loops Worksheet 6.py
@@ -4,9 +4,8 @@ for i in range (11):
     print(i)
 
 #2
-for i in range (10, 21, +1):
-    if (i % 2 == 0):
-        print(i + i)
+for i in range(10,21,2):
+    print(i,end = " ")
     
 #3
 for i in range (10, 0, -1):
@@ -17,11 +16,19 @@ for i in range (10, 0, -1):
 for i in range (11):
         print(i, 'squared is:', i*i, ', and cubed is:', i*i*i)
 
-#5
-
-
-#6
-
+#5 and #6
+length = int(input('How many rows do you want?'))
+for i in range(length + 1):
+    for j in range(i):
+        print('*',end=' ')
+    print()
 
 #7
-
+listNummbers = []
+for i in range(5):
+    listNummbers.append(int(input('Give me a number: ')))
+answer = 1    
+for j in listNummbers:
+    answer *= j
+print(answer)    
+        
