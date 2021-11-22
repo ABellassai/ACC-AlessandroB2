ac0c4f90fc9d980fa4ecb71bd45a0f4cb4c44a0b
ABellassai.acc <92167252+ABellassai@users.noreply.github.com>
2021-11-22 09:33:44 +0000

    Add files via upload

---
+++ CS Lists Worksheet .py
@@ -0,0 +1,25 @@
+#1
+list1 = []
+print(list1) #This is Empty like ME!!!
+
+#2
+
+list = ['You mom', 'buys me icecream']
+list[i] =  #stores x with index i
+list[i] #retrieves the item with index i
+list[-i] #retrieves last i item from list
+list[i:j] #retrieves items in the range i to j
+list[i:] #retrieves items from i to the end
+del list[i] #removes the item with index i
+list.append(x) #appends x to the end of the list
+list.extend(L) #appends L to the end of the list
+list.insert(i,x) #inserts x at i position
+list.remove(x) #removes the first list item whose value is x
+list.pop(i) #removes the item at position i and returns its value
+list.clear() #removes all items from the list
+list.index(x) #returns the position of the first occurrence of x in a list
+list.count(x) #returns the number of times x appears in a list
+list.reverse() #reverses list elements
+list.copy() #returns a copy of the list
+
+#3
