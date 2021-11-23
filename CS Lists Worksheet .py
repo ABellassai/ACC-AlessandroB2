8d6782f4081e157c847afbce128c8e60a2367751
ABellassai.acc <92167252+ABellassai@users.noreply.github.com>
2021-11-23 21:30:25 +0000

    Update CS Lists Worksheet .py

--- CS Lists Worksheet .py
+++ CS Lists Worksheet .py
@@ -4,22 +4,34 @@ print(list1) #This is Empty like ME!!!
 
 #2
 
-list = ['You mom', 'buys me icecream']
-list[i] =  #stores x with index i
-list[i] #retrieves the item with index i
-list[-i] #retrieves last i item from list
-list[i:j] #retrieves items in the range i to j
-list[i:] #retrieves items from i to the end
-del list[i] #removes the item with index i
-list.append(x) #appends x to the end of the list
-list.extend(L) #appends L to the end of the list
-list.insert(i,x) #inserts x at i position
-list.remove(x) #removes the first list item whose value is x
-list.pop(i) #removes the item at position i and returns its value
+list = [1,2,3,4,5,6,7,8,9,10]
+list[3] = 3 #stores x with index i
+list[5] #retrieves the item with index i
+list[-7] #retrieves last i item from list
+list[5:9] #retrieves items in the range i to j
+list[4:] #retrieves items from i to the end
+del list[2] #removes the item with index i
+list.append(11) #appends x to the end of the list
+list.extend(list1) #appends L to the end of the list
+list.insert(13,15) #inserts x at i position
+list.remove(6) #removes the first list item whose value is x
+list.pop(1) #removes the item at position i and returns its value
 list.clear() #removes all items from the list
-list.index(x) #returns the position of the first occurrence of x in a list
-list.count(x) #returns the number of times x appears in a list
+list.index(7) #returns the position of the first occurrence of x in a list
+list.count(8) #returns the number of times x appears in a list
 list.reverse() #reverses list elements
-list.copy() #returns a copy of the list
+list.copy(list1) #returns a copy of the list
+print(list1)
 
 #3
+list2=["a", "b", "c", "d"]
+print(list2)
+print("""1. Append an element
+2. Insert an element
+3. Append a list to the given list
+4. Modify an existing element
+5. Delete an existing element from its position
+6. Delete an existing element with a given value
+7. Sort the list in the ascending order
+8. Sort the list in descending order
+9. Display the list.""")
