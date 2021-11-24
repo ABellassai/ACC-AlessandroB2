111b1cb1101e32e514c628fa5161fcb1f6fd26d8
ABellassai.acc <92167252+ABellassai@users.noreply.github.com>
2021-11-24 14:23:58 +0000

    Update CS Lists Worksheet .py

--- CS Lists Worksheet .py
+++ CS Lists Worksheet .py
@@ -1,10 +1,11 @@
+
 #1
 list1 = []
 print(list1) #This is Empty like ME!!!
 
 #2
 
-list = [1,2,3,4,5,6,7,8,9,10]
+list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 list[3] = 3 #stores x with index i
 list[5] #retrieves the item with index i
 list[-7] #retrieves last i item from list
@@ -16,16 +17,16 @@ list.extend(list1) #appends L to the end of the list
 list.insert(13,15) #inserts x at i position
 list.remove(6) #removes the first list item whose value is x
 list.pop(1) #removes the item at position i and returns its value
-list.clear() #removes all items from the list
-list.index(7) #returns the position of the first occurrence of x in a list
+print(list.index(10)) #returns the position of the first occurrence of x in a list
 list.count(8) #returns the number of times x appears in a list
 list.reverse() #reverses list elements
 list.copy(list1) #returns a copy of the list
+list.clear() #removes all items from the list
 print(list1)
 
 #3
-list2=["a", "b", "c", "d"]
-print(list2)
+aList=["a", "b", "c", "d"]
+print(aList)
 print("""1. Append an element
 2. Insert an element
 3. Append a list to the given list
