f0f8ef9417b66960a19b3f143b7e47e2ec6430b5
ABellassai.acc <92167252+ABellassai@users.noreply.github.com>
2021-11-25 20:17:53 +0000

    Update CS Lists Worksheet 9 .py

--- CS Lists Worksheet 9 .py
+++ CS Lists Worksheet 9 .py
@@ -29,6 +29,20 @@ print(list5) #output [2,3,5,7,9]
 list5.sort(reverse = True) # returns a new list with L items sorted 
 print(list5) # output [9,7,5,3,2]
 
+#3
+aList = ['a', 'b', 'c', 'd'] 
+print(aList)
+print("""1. Append an element
+2. Insert an element
+3. Append a list to the given list
+4. Modify an existing element
+5. Delete an existing element from its position
+6. Delete an existing element with a given value
+7. Sort the list in the ascending order
+8. Sort the list in descending order
+9. Display the list
+Please enter an option(1 - 9): """)
+
 choiceMade = int(input())
 if choiceMade == 1:
     print('1. Append an element')
@@ -41,7 +55,7 @@ elif choiceMade == 2:
     print('2. Insert an element')
     print('Current List = [ a, b, c, d] ')
     insert = int(input('Insert an element to replace another element: '))
-    sobstitute = int(input('What do you want to sobstitute ? '))
+    sobstitute = input('What do you want to sobstitute ? ')
     aList.insert(insert , sobstitute)
     print(aList)
 
@@ -49,39 +63,53 @@ elif choiceMade == 2:
 elif choiceMade == 3:
     print('3. Append a list to the given list')
     print('Current List = [ a, b, c, d] ')
-    aList.extend(user_list)
+    value1 = str(input('Enter a value: '))
+    value2 = str(input('Enter a second value: '))
+    value3 = str(input('Enter a third value: '))
+    value4 = str(input('Enter one last value: '))
+    newList = [value1, value2, value3, value4]
+    aList.extend(newList)
+    print(aList)
 
 
 elif choiceMade == 4:
-    print('1. Append an element')
+    print('4. Modify an existing element')
     print('Current List = [ a, b, c, d] ')
-    aList[i] = x
-
+    modify = int(input('What element would you want to modify? '))
+    modifyDone = str(input('What do you want it to become? '))
+    aList[modify] = modifyDone
+    print(aList)
 
 elif choiceMade == 5:
-    print('1. Append an element')
+    print('5. Delete an existing element from its position')
     print('Current List = [ a, b, c, d] ')
-    del aList[i]
+    aList.remove("d")
+    print('The new list is: ',aList)
 
 
 elif choiceMade == 6:
-    print('1. Append an element')
+    print('6. Delete an existing element with a given value')
     print('Current List = [ a, b, c, d] ')
-    aList.pop(i)
+    deleteElement = int(input('Enter a element to remove: '))
+    aList.pop(deleteElement)
+    print('The new list is: ', aList)
 
 
 elif choiceMade == 7:
-    print('1. Append an element')
+    print('7. Sort the list in the ascending order')
     print('Current List = [ a, b, c, d] ')
     aList.sort()
+    print(aList)
 
 elif choiceMade == 8:
-    print('1. Append an element')
+    print('8. Sort the list in descending order')
     print('Current List = [ a, b, c, d] ')
     aList.sort(reverse = True)
     print(aList)
 
 elif choiceMade == 9:
-    print('1. Append an element')
+    print('9. Display the list')
     print('Current List = [ a, b, c, d] ')
     print(aList)
+
+    
