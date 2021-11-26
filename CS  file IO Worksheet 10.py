26fcaa959329702276dbbca095f3f801ef13aaef
ABellassai.acc <92167252+ABellassai@users.noreply.github.com>
2021-11-26 15:00:42 +0000

    Add files via upload

---
+++ CS  file IO Worksheet 10.py
@@ -0,0 +1,22 @@
+#1
+# Write a program to open the files and print each line:
+# Range, Frequency, Mean, Median, and Mode values for each file
+
+file1 = open("C:/Users/18ABellassai.ACC/Downloads/10Random.txt", "r")
+file2 = open("C:/Users/18ABellassai.ACC/Downloads/100Random.txt", "r")
+file1.read(100)
+file2.read(10)
+
+#The range tells us how spread out a group of numbers are. To calculate the range we subtract the
+#smallest number from the largest.
+
+
+#The frequency is a count of how often each different number appears
+
+
+#The mean is the average of all the numbers. To calculate the mean, add all the numbers together
+#and divide this value by the number of numbers
+
+
+#The mode is the number which appears most often. If two numbers occur with the same frequency,
+#the data is bi-modal, and more than two is multimodal
