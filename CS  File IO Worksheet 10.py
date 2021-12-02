12327d4e8b58ebed5c58fcb6e5225e5d935bfcf7
ABellassai.acc <92167252+ABellassai@users.noreply.github.com>
2021-12-02 12:33:22 +0000

    Update CS  File IO Worksheet 10.py

--- CS  File IO Worksheet 10.py
+++ CS  File IO Worksheet 10.py
@@ -4,9 +4,10 @@
 
 file1 = open("C:/Users/18ABellassai.ACC/Downloads/10Random.txt", "r")
 file2 = open("C:/Users/18ABellassai.ACC/Downloads/100Random.txt", "r")
+'''
 print(file1.read())
 print(file2.read())
-
+'''
 #The range tells us how spread out a group of numbers are. To calculate the range we subtract the
 #smallest number from the largest
 
@@ -14,20 +15,31 @@ list1 = file1.read()
 final_List = []
 for item in list1.split():
     final_List.append(int(item))
+'''
 print(final_List)
 final_List.sort()
-print(final_List[9] - final_List[0])
-
+print(final_List[-1] - final_List[0])
+'''
 list2 = file2.read()
 final_List2 = []
 for item2 in list2.split():
     final_List2.append(int(item2))
+'''
 print(final_List2)
 final_List2.sort()
-print(final_List2[99] - final_List2[0])
-
+print(final_List2[-1] - final_List2[0])
+'''
 #The frequency is a count of how often each different number appears
 
+counter = 0
+for item in list1.count(item):
+        counter + 1
+final_List.count(item)
+print(final_List, ':', counter)
+
+
+
+final_List2.count
 
 #The mean is the average of all the numbers. To calculate the mean, add all the numbers together
 #and divide this value by the number of numbers
