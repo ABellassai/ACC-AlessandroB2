0c385de7efbd9eeb8e51d3ffd5bc816f7d1b582e
ABellassai.acc <92167252+ABellassai@users.noreply.github.com>
2021-12-08 15:13:52 +0000

    Add files via upload

---
+++ CS File IO Worksheet 11.py
@@ -0,0 +1,61 @@
+#1 Write a Python function to find the Max of two numbers input by the user
+'''
+def findMax():
+    num1 = int(input('Enter a saucy number: '))
+    num2 = int(input('Enter another saucy number: '))
+    if num1 < num2:
+        print (num2, 'is bigger than', num1)
+    else:
+        print (num1, 'is bigger than', num2)
+findMax()
+
+
+def findMax(n1, n2):
+    if num1 < num2:
+        print (num2, 'is bigger than', num1)
+    else:
+        print (num1, 'is bigger than', num2)
+num1 = int(input('Enter a saucy number: '))
+num2 = int(input('Enter another saucy number: '))
+    
+findMax(num1, num2)
+'''
+#2 Write a Python function to multiply two numbers input by the user
+'''
+def multiplyNum():
+    num1 = int(input('Enter a saucy number: '))
+    num2 = int(input('Enter another saucy number: '))
+    print (num1, 'multiplied by', num2, 'is', num1*num2)
+multiplyNum()
+
+
+def multiplyNum(n1, n2):
+    print (num1, 'multiplied by', num2, 'is', num1*num2)
+num1 = int(input('Enter a saucy number: '))
+num2 = int(input('Enter another saucy number: '))
+
+multiplyNum(num1, num2)
+'''
+#3 Write a Python program to print a string backwards – string will be input
+def modifiedStr():
+    string = str(input('Enter a saucy string: '))
+    
+
+def modifiedStr(s1):
+    
+    
+string = str(input('Enter a saucy string: '))
+def modifiedStr(string):
+
+#4 Write a Python program to print the even numbers from a given list –the list can be user defined in the code
+def evenNumList():
+    
+    
+evenNumList()
+
+
+def evenNumList(n1, n2):
+    
+
+evenNumList(num1, num2)
+
