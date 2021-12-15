98cb3349ddb8068cd50595ab2c8bdffe867afede
ABellassai.acc <92167252+ABellassai@users.noreply.github.com>
2021-12-15 15:12:00 +0000

    Add files via upload

---
+++ ChristmasTest2021.py
@@ -0,0 +1,54 @@
+'''
+#program 1
+list1 = [23, 1, 0, -12, 48]
+sum1 = 0
+for i in list1:
+    sum1= sum1+i
+print(sum1)
+    
+#program 2
+list2 = [23, 1, 0, -12, 48]
+sum2 = 0
+for i in list2:
+    if i%2 == 1:
+        sum2= sum2+i
+print(sum2)
+
+#program 3
+list3 = [23, 1, 0, -12, 48]
+counter = 0
+for i in list3:
+    if i%2 == 0:
+     counter = counter+1
+print(counter)
+
+#program 4
+list4 = [23, 1, 0, -12, 48]
+products = 0
+for i in list4:
+    i * products+1
+print(products)
+
+#program 5
+
+list5 = [23, 1, 0, -12, 48]
+products = 0
+for i in list5:
+    '''
+#program 6
+string= 'cat'
+times = 0
+while times < len(string) :
+    print(string[times])
+    times = times + 1          
+#program 7
+list7 = [1, 2, 3, 4, 5]
+sum7 = 0
+for i in list7:
+    i+1
+    sumOfAll = sum7 + i
+print(sumOfAll)
+
+
+#program 8
+
