6167ddb67df23f378f4614a07af0a96e4ad70290
ABellassai.acc <92167252+ABellassai@users.noreply.github.com>
2021-12-15 19:52:49 +0000

    Update ChristmasTest2021.py

--- ChristmasTest2021.py
+++ ChristmasTest2021.py
@@ -1,10 +1,11 @@
-'''
+f = open('AlessandroBellassaiChristmasTest2021.txt' ,'w')
+
 #program 1
 list1 = [23, 1, 0, -12, 48]
 sum1 = 0
 for i in list1:
     sum1= sum1+i
-print(sum1)
+f.write(str(sum1)+ '\n')
     
 #program 2
 list2 = [23, 1, 0, -12, 48]
@@ -12,7 +13,7 @@ sum2 = 0
 for i in list2:
     if i%2 == 1:
         sum2= sum2+i
-print(sum2)
+f.write(str(sum2)+ '\n')
 
 #program 3
 list3 = [23, 1, 0, -12, 48]
@@ -20,35 +21,44 @@ counter = 0
 for i in list3:
     if i%2 == 0:
      counter = counter+1
-print(counter)
+f.write(str(counter)+ '\n')
 
 #program 4
 list4 = [23, 1, 0, -12, 48]
 products = 0
 for i in list4:
     i * products+1
-print(products)
+f.write(str(products)+ '\n')
 
 #program 5
+num = 20
+interval = 4
+for counter in range(interval, num + 1, interval):
+    f.write(str(counter) + '\n')
 
-list5 = [23, 1, 0, -12, 48]
-products = 0
-for i in list5:
-    '''
 #program 6
 string= 'cat'
 times = 0
 while times < len(string) :
-    print(string[times])
-    times = times + 1          
-#program 7
-list7 = [1, 2, 3, 4, 5]
-sum7 = 0
-for i in list7:
-    i+1
-    sumOfAll = sum7 + i
-print(sumOfAll)
+    f.write(string[times]+ '\n')
+    times = times + 1
 
+#program 7
+num = 5
+result = 0
+counter = 0
+while counter < 5:
+    counter = counter+1
+    result= result + counter
+f.write(str(result)+ '\n')
 
 #program 8
-
+list8 = [1,2,3,2,5]
+value = 2
+counter = 0
+while counter < len(list8):
+    if value == list8[counter]:
+        f.write('The value ' + str(value) + ' is at list8[' + str(counter) + ']' + '\n')
+    counter = counter+1
+    
+f.close()
