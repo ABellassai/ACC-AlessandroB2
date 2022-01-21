5c2c16f474dd1185b5509bd1bc25f53378615a05
ABellassai.acc <92167252+ABellassai@users.noreply.github.com>
2022-01-21 14:35:06 +0000

    Update CS Insertion sort Worksheet 13.py

--- CS Insertion sort Worksheet 13.py
+++ CS Insertion sort Worksheet 13.py
@@ -2,24 +2,36 @@
 myList = [5, 7, 3, 6, 2, 4, 1]
 
 #Bubble Sort Function
-def bubbleSort(L, descending = False, dbg = True):
-   # print("INPUT (initial list): ", L)
+def bubbleSort(L, descending = True, dbg = False):
     exchange = True
     n = len(L)
     i = 0
     while (i< n) and  exchange:
-       # print("BEFORE PASS %d: %s " %(i+1, L))
         exchange = False
-        for j in range(n-i-1):
-            #print("%s " %L, end="-> ")
-            if L[j] > L[j+1]:
-                L[j], L[j+1] = L[j+1], L[j] 
-                exchange = True 
-            #print("%s " %L)
+        for j in range(n-i-1): 
+            if descending == False:
+                if L[j] > L[j+1]:
+                    L[j], L[j+1] = L[j+1], L[j] 
+                    exchange = True      
+            if descending == True:
+                if L[j] < L[j+1]:
+                    L[j], L[j+1] = L[j+1], L[j] 
+                    exchange = True    
             
-        #print("AFTER PASS %d: %s " %(i+1, L))
+        if dbg == True:
+            file1 = open('C:/Users/18ABellassai.ACC/Downloads/debug log.txt')
+            print('Debug Info')
+            print("BEFORE PASS %d: %s " %(i+1, L))
+            print("%s " %L, end="-> ")
+            print("%s " %L)
+            print("AFTER PASS %d: %s " %(i+1, L))
+            file1.close
+            
+        elif dbg == False:
+            ('')
+            
         i= i+1
-    return(L) #("OUTPUT (sorted list): ", L)
+    return(L) 
 result = bubbleSort(myList)
 print(result)
 
@@ -40,7 +52,7 @@ print(result)
 
 #Program 2
 myList2 = [9, 2, 3, 1, 5, 6, 7, 3, 4, 5]
-sortSelection = int(input('Type 1 for Bubblem sort or 2 for Insertion sort: '))
+sortSelection = int(input('Type 1 for Bubble sort or 2 for Insertion sort: '))
 
 if sortSelection == 1:
     result = bubbleSort(myList2)
