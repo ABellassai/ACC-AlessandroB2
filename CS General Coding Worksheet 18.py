7a67d5c2757f00b20cfa7d00e46c721b52e73e9d
ABellassai.acc <92167252+ABellassai@users.noreply.github.com>
2022-02-16 15:13:40 +0000

    Add files via upload

---
+++ CS General Coding Worksheet 18.py
@@ -0,0 +1,45 @@
+# 1. Write a function that returns the largest element in a list
+import random
+list1 = []
+for a in range (0,5):
+    n = random.randint(1,50)
+    list1.append(n)
+print(list1)
+
+# 2. Write function that reverses a list in place – this means you don’t create a new list
+
+
+# 3. Write a function that checks whether an element occurs in a list
+
+
+# 4. Write a function that returns the elements in odd positions in a list
+
+
+# 5. Write a function that computes the running total of a list
+
+
+# 6. Write a function that tests whether a string is a palindrome
+
+
+# 7. Write three functions that compute the sum of the numbers in a list: using a
+# for-loop, a while-loop and recursion
+
+
+# 8. Write a function perfect_square that to print the first n perfect squares. The
+# perfect squares can be found by multiplying each natural number with itself.
+# The first few perfect squares are 1*1= 1, 2*2=4, 3*3=9, 4*4=16.
+# n is input by the user
+
+
+
+# 9. Write a function that concatenates two lists. [a,b,c], [1,2,3] → [a,b,c,1,2,3]
+
+
+
+# 10.Write a function that combines two lists by alternatingly taking elements, e.g.
+# [a,b,c], [1,2,3] → [a,1,b,2,c,3].
+
+
+
+# 11.Write a function that merges two sorted lists into a new sorted list.
+# [1,3,6],[2,4,5] → [1,2,3,4,5,6].
