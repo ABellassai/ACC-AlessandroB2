c98fea451237f8876dfcc71355fdeecd2bbaaae8
ABellassai.acc <92167252+ABellassai@users.noreply.github.com>
2022-04-04 09:40:57 +0100

    Update CS Unicode to UTf-8.py

--- CS Unicode to UTf-8.py
+++ CS Unicode to UTf-8.py
@@ -1,6 +1,40 @@
 #Program 1
 # Write a python program to encode a Unicode code point value as UTF-8
-input('GIVE ME A : ')
+o = chr(int('1010000', 2))
+print (o)
 
-
+value = 0
+if value == 10:
+    value == A
+elif value == 11:
+    value == B
+elif value == 12:
+    value == C
+elif value == 13:
+    value == D
+elif value == 14:
+    value == E
+elif value == 15:
+    value == F
+    
+    
 #Program 2
+# Write a second program which decodes UTF-8 values to their Unicode code point and print
+# the character at that code point
+c = ord('P')
+print (c)
+
+
+value = 0
+if value =='A':
+    value == 10
+elif value == 'B':
+    value == 11
+elif value == 'C':
+    value == 12
+elif value == 'D':
+    value == 13
+elif value == 'E':
+    value == 14
+elif value == 'F':
+    value == 15
