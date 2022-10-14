4b50ac01f530211a01b2ec47eb00a155a4a2f073
ABellassai.acc <92167252+ABellassai@users.noreply.github.com>
2022-10-14 12:57:34 +0100

    Update CS recursive functions Worksheet 15.py

--- CS recursive functions Worksheet 15.py
+++ CS recursive functions Worksheet 15.py
@@ -1,20 +1,29 @@
 #1
-myList = [1,2,3,4,5,6,7]
-
-def myListsum(aList):
-    if len(aList) == 1:
-        return aList[0]
+n = int(input('Give me a fatorial number: '))
+def factorial(n):
+    if(n == 1 or n == 0):
+        return 1
     else:
-        return aList[0] + myListsum(aList[1:])
-
-print(myListsum(myList))
+        return n*factorial(n-1)
+print(factorial(n))
 
 #2
-def sum1(a):
-    if a < 10:
-        return a
+number = 9
+def series(number):
+    if(number <= 1):
+        return 1
     else:
-        return a % 10 + sum1(a//10)
-n = 1234567
+        return series(number-1) + series(number-2)
+print('series: ')
+for i in range (number):
+        print(series(i))
 
-print(sum1(n))
+#3
+num = int(input('Give me a number: '))
+powerNum = int(input('Give me a power: ')) 
+def power(num):
+    if(num == 1 or num == 0):
+        return 1
+    else:
+        return num**powerNum
+print(power(num))
