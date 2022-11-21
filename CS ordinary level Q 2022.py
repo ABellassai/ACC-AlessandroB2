5cb1c9e6ffb679e5e7c2476ae0dc489d52631c57
ABellassai.acc <92167252+ABellassai@users.noreply.github.com>
2022-11-21 18:28:40 +0000

    Update CS ordinary level Q 2022.py

--- CS ordinary level Q 2022.py
+++ CS ordinary level Q 2022.py
@@ -7,14 +7,17 @@ age = int(input('Enter your age: '))
 eircode = []
 eirc = input('Enter your Eircode: ')
 for i in eirc:
-    eircode = eircode.append(i)
+    eircode.append(i)
 print("Hello", f_name, s_name, 'You are', age, 'years old')
 if age >= 12 and age <= 49:
     print(f_name,'you will receive the MRNA vaccine')
 elif age > 49:
     print(f_name,'you will receive the ADENO vaccine')
-print('Your Earcode is', eirc)
-if eircode[-1] % 2 == 0:
-    print('You must attend Eastwood for your vaccine')
-else:
-    print('You must attend Northfield for your vaccine')
+print('Your Earcode is', eircode)
+value = eircode[-1]
+print(value)
+if value.isdigit() == True:
+    if value % 2 == 0:
+        print('You must attend Eastwood for your vaccine')
+    else:
+        print('You must attend Northfield for your vaccine')
