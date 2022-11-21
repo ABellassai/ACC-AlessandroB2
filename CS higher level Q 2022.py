57cbcddc4e463416dc9af786f76f651d8d09e837
ABellassai.acc <92167252+ABellassai@users.noreply.github.com>
2022-11-21 23:13:32 +0000

    Add files via upload

---
+++ CS higher level Q 2022.py
@@ -0,0 +1,6 @@
+# Question 16(a)
+# Name and School: Alessandro Bellassai
+
+f = open('C:/home/Scaricati/2022 DEB Mock/2022 L.89 Higher Level Seed files for Section C/shelley.txt', 'r')
+print(f.read())
+f.close()
