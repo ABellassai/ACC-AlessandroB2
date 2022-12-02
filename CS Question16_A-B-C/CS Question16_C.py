1ab811639b658bfda8d3c931b9eadce0494e13a8
ABellassai.acc <92167252+ABellassai@users.noreply.github.com>
2022-12-02 11:07:25 +0000

    Update CS Question16_C.py

--- CS Question16_A-B-C/CS Question16_C.py
+++ CS Question16_A-B-C/CS Question16_C.py
@@ -5,19 +5,22 @@ bmi_values = [24, 19, 33, 35, 27, 18, 15, 33, 35, 23, 32, 23]
 counter = 0
 obeseNum = 0
 
-for k in range(len(bmi_values)): 
-    for y in range(len(bmi_values) - 1):
-        if bmi_values[y] < bmi_values[y + 1]:
-            temp = bmi_values[y]
-            bmi_values[y] = bmi_values[y + 1]
-            bmi_values[y + 1] = temp
-            
 for i in bmi_values:
     if i >= 30:
         obeseNum = obeseNum + 1
     counter = counter +1  
 
 print('Obese:',obeseNum)
-print('Largest number:', bmi_values[0])
-print('Second largest number:', bmi_values[1])
-
+    
+def find_nth_largest(n, list_of_values): 
+    for k in range(len(list_of_values)): 
+        for y in range(len(list_of_values) - 1):
+            if list_of_values[y] < list_of_values[y + 1]:
+                temp = list_of_values[y]
+                list_of_values[y] = list_of_values[y + 1]
+                list_of_values[y + 1] = temp
+                
+    return list_of_values[n]
+num = int(input('Enter the nth value you wish to find: '))    
+list_of_values = find_nth_largest(num, bmi_values)
+print(list_of_values)
